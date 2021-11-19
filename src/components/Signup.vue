91cd9b21b078f9bb21abd88b96b4fc71439e7c51
石丸大貴 <hirokiishimaru6132@gmail.com>
2021-11-19 16:21:20 +0900

    パスワード確認完成

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -14,6 +14,12 @@
         <tr>
           <td><input type="password" v-model="password"/></td>
         </tr>
+        <tr>
+          <th>パスワード確認：</th>
+        </tr>
+        <tr>
+          <td><input type="password" v-model="repassword"/></td>
+        </tr>
     </table>
     <button @click="signUp">登録</button>
   </div>
@@ -23,26 +29,31 @@
 // import firebase from 'firebase/compat/app';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 export default {
-    data () {
-        return {
-        email: '',
-        password: ''
-        }
-    },
-    methods: {
-        signUp: function () {
-          const auth = getAuth();
-          createUserWithEmailAndPassword(auth, this.email, this.password)
-            .then((userCredential)=>{
-              const user = userCredential.user;
-              console.log(user, "成功")
-            })
-            .catch((error)=>{
-              const errorCode = error.code;
-              const errorMessage = error.message;
-              console.log(errorCode, errorMessage, "失敗")
-            })
-        }
+  data () {
+      return {
+      email: '',
+      password: '',
+      repassword: '',
+      }
+  },
+  methods: {
+    signUp: function () {
+      const auth = getAuth();
+      if(this.password === this.repassword){
+        createUserWithEmailAndPassword(auth, this.email, this.password, this.repassword)
+          .then((userCredential)=>{
+            const user = userCredential.user;
+            console.log(user, "成功")
+          })
+          .catch((error)=>{
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage, "失敗")
+          })
+      }else{
+        alert("パスワード不一致");
+      }
     }
+  }
 }
 </script>
