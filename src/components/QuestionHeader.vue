93486aba4ac8d0b036bae53a4a6de3c06caac98e
renmurakami <autoeyy@gmail.com>
2021-12-03 15:36:42 +0900

    test

---
+++ src/components/QuestionHeader.vue
@@ -0,0 +1,25 @@
+<template>
+	<header>
+		<div class="header">
+			<img class="header__img" src="@/assets/logo.svg" alt="" />
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "QuestionHeader",
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+	background-color: tomato;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 50vh;
+	&__img {
+	}
+}
+</style>
---
+++ src/views/questions/QuestionIndex.vue
@@ -0,0 +1,14 @@
+<template>
+	<QuestionHeader />
+</template>
+
+<script>
+import QuestionHeader from "@/components/QuestionHeader.vue";
+export default {
+	components: {
+		QuestionHeader,
+	},
+};
+</script>
+
+<style scoped lang="scss"></style>
