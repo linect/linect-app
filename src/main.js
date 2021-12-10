3cd5d1ebb279d2e98487e7addda1da205837573c
renmurakami <autoeyy@gmail.com>
2021-12-10 15:36:38 +0900

    main.js変更

--- src/main.js
+++ src/main.js
@@ -1,30 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import firebase from 'firebase/compat/app'; 
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import firebase from "firebase/compat/app";
 import "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+
+require("./sass/main.scss");
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDewhwZcwmfYLgcrewgCHKfMs-XnRXk9Nc",
-  authDomain: "linect-bc1f0.firebaseapp.com",
-  projectId: "linect-bc1f0",
-  storageBucket: "linect-bc1f0.appspot.com",
-  messagingSenderId: "518504299720",
-  appId: "1:518504299720:web:7d02150e2102c36bceb9fd",
-  measurementId: "G-D003ZZN26B"
+	apiKey: "AIzaSyDewhwZcwmfYLgcrewgCHKfMs-XnRXk9Nc",
+	authDomain: "linect-bc1f0.firebaseapp.com",
+	projectId: "linect-bc1f0",
+	storageBucket: "linect-bc1f0.appspot.com",
+	messagingSenderId: "518504299720",
+	appId: "1:518504299720:web:7d02150e2102c36bceb9fd",
+	measurementId: "G-D003ZZN26B",
 };
 const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  app,
-  firebase,
-  render: h => h(App)
-}).$mount('#app')
-
+	router,
+	store,
+	app,
+	firebase,
+	render: (h) => h(App),
+}).$mount("#app");
